a91e74ca8563d4e9ddcc9109996d537411896cb7
Lukas Stejskal <lucastej@gmail.com>
2012-09-11 12:06:16 +0200

    Removed obsolete Error.is_error method

--- lib/oauth_doorman/oauth_error.rb
+++ lib/oauth_doorman/oauth_error.rb
@@ -3,20 +3,18 @@ class OauthError < Exception; end
 
 module OauthDoorman
   module Error
-    def is_error(request_result)
-      error_regex = /error/
-      return error_regex.match(request_result)
-    end
 
     def process_error(request_result)
-      
       if request_result == nil
         raise OauthError, "nil response content"
       end
 
       json = JSON.parse(request_result) rescue nil
-      raise OauthError, json if json && json.has_key?("error")
 
+      if json && json.has_key?("error")
+        raise OauthError, json
+      end 
     end
+
   end
 end
